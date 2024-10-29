e57618a2d9dc4ec275a753fb8cd08dc6ef236421
troyflowrest <dbegvc110195@gmail.com>
2024-10-29 17:50:09 +0800

    applya mailhog and mailing foundation

---
+++ backend/src/main/java/com/thesis/carrental/constants/EmailTemplates.java
@@ -0,0 +1,6 @@
+package com.thesis.carrental.constants;
+
+public class EmailTemplates {
+    public static final String FROM_EMAIL = "carrental@email.com";
+    public static final String REG_SUCCESS_TEMPLATE = "test.html";
+}
